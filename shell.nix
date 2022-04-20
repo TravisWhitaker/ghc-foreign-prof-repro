29ed793a80c469e7a8848e83ba98edd3a002735d
Travis Whitaker <pi.boy.travis@gmail.com>
2022-04-19 17:57:49 -0700

    Make sure we have prof base.

--- shell.nix
+++ shell.nix
@@ -4,10 +4,14 @@ let pkgsrc = builtins.fetchTarball
         sha256 = "0g880gbkxf9n80pkz2nkf177naxf6n95qkca1ca7m9nk32gvki0f";
     };
     pkgs = import pkgsrc {};
+    hspkgs = (pkgs.haskell.packages.ghc8107.override
+    {
+        overrides = self: super:
+        {
+            enableLibraryProfiling = true;
+        };
+    }).ghcWithPackages (p: [p.cabal-install]);
 in pkgs.mkShell
 {
-    packages =
-    [
-        (pkgs.haskell.packages.ghc8107.ghcWithPackages (p: [p.cabal-install]))
-    ];
+    packages = [hspkgs];
 }
